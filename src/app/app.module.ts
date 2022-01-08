cf1c98327e3beefa524045a7c096ee90c1e0bb6f
cornelious <cornelioust@dipar.co.za>
2022-01-08 07:01:58 +0200

    dat binding and pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AgencyListComponent } from './agencies/agency-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AgencyListComponent } from './agencies/agency-list.component';
     AgencyListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
