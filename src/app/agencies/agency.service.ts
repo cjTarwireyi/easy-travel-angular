2fa80f6fe68c33ed61eeff43c738f847060818eb
cornelious <cornelioust@dipar.co.za>
2022-01-09 10:50:12 +0200

    retrieving data using http

--- src/app/agencies/agency.service.ts
+++ src/app/agencies/agency.service.ts
@@ -1,41 +1,35 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import {catchError, tap} from "rxjs/operators"
 import { IAgency } from "./agency-list.interface";
 
 @Injectable({
     providedIn:'root'
 })
 export class AgencyService{
-    getAgencies(): IAgency[]{
-        return[
-            {
-                agencyId:1,
-                agencyName:"Mukanya Coach",
-                agencyCode:"MC-001",
-                starRating:4.1,
-                available:"Y",
-                industry:"Pasenger Transport",
-                logoUrl: "assets/agencyLogos/mk.jpeg"
-            },
-            {
-                agencyId:1,
-                agencyName:"Shumba Goods Transport",
-                agencyCode:"SG-001",
-                starRating:4.5,
-                available:"Y",
-                industry:"Goods Transport",
-                logoUrl: "assets/agencyLogos/sg.png"
-            },
-            {
-                agencyId:1,
-                agencyName:"Nzou Logistic",
-                agencyCode:"NZ-001",
-                starRating:3,
-                available:"Y",
-                industry:"Mixed",
-                logoUrl: "assets/agencyLogos/nz.png"
-            }
-    
-        ]
+    private agencyUrl ='api/agencies/agencies.json'
+    constructor(private http: HttpClient){
+        
     }
+    getAgencies(): Observable<IAgency[]>{
+        return this.http.get<IAgency[]>(this.agencyUrl).pipe( tap( data => console.log('All', JSON.stringify(data))),
+        catchError(this.handleError))
+    }
+    private handleError(err: HttpErrorResponse): Observable<never> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      }
 
 }
