fb0139d7989bcaf18d3ad8518bae41acd916f916
cornelious <cornelioust@dipar.co.za>
2022-01-11 20:44:40 +0200

    angular modules including shared modules

---
+++ src/app/agencies/agency.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { AgencyListComponent } from './agency-list.component';
+import { AgencyDetailComponent } from './agency-detail.component';
+import { RouterModule } from '@angular/router';
+import { AgencyDetailGuard } from './agency-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AgencyListComponent,     
+    AgencyDetailComponent
+  ],
+  imports: [    
+    RouterModule.forChild([
+      { path: 'agencies', component: AgencyListComponent},
+      { path: 'agencies/:id', component: AgencyDetailComponent, canActivate:[AgencyDetailGuard]},
+    ]),
+    SharedModule
+  ]
+})
+export class AgencyModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AgencyListComponent } from './agencies/agency-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { AgencyDetailComponent } from './agencies/agency-detail.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
-import { AgencyDetailGuard } from './agencies/agency-detail.guard';
 import { jqxBarGaugeModule } from 'jqwidgets-ng/jqxbargauge';
+import { AgencyModule } from './agencies/agency.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AgencyListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    AgencyDetailComponent,
+    AppComponent,  
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     jqxBarGaugeModule,
-    RouterModule.forRoot([
-      { path: 'agencies', component: AgencyListComponent},
-      { path: 'agencies/:id', component: AgencyDetailComponent, canActivate:[AgencyDetailGuard]},
+    RouterModule.forRoot([      
       { path: 'home', component: HomeComponent},
       { path: '', redirectTo: 'home', pathMatch:'full'},
       { path: '**', redirectTo: 'home', pathMatch:'full'}
-    ])
+    ]),
+    AgencyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
