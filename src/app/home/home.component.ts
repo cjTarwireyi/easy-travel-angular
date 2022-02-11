a6af0018739e50146868c1cf6738d210f4523605
cornelious <cornelioust@dipar.co.za>
2022-02-11 13:54:47 +0200

    Home page design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { IAgency } from '../agencies/agency-list.interface';
+import { AgencyService } from '../agencies/agency.service';
 
 
 @Component({
@@ -7,9 +9,14 @@ import { Subscription } from 'rxjs';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  public pageTitle = 'Welcome to Home Page';  
+  public pageTitle = 'Welcome to Home Page'; 
+  longText = `A travel agency is a private retailer or public service that provides travel
+   and tourism-related services to the general public on behalf of accommodation or travel
+    suppliers   to offer different kinds of travelling packages for each destination.`; 
   sub!: Subscription;
-  showDefaultCard: boolean =false;
+  errorMessage:  string = '';
+  showDefaultCard: boolean = false;
+  agencies: IAgency[] = [ ];  
   tooltip: any =
     {
         visible: true,
@@ -20,8 +27,17 @@ export class HomeComponent {
     }
     values: number[] = [102, 115, 130, 140];
 
+    constructor(private agencyService: AgencyService){
+      
+  }
+
     ngOnInit(): void {     
-  
+      this.sub = this.agencyService.getAgencies().subscribe({
+        next: agencies => {
+            this.agencies = agencies;           
+        },
+        error: err => this.errorMessage = err
+    });
          
       }
 }
